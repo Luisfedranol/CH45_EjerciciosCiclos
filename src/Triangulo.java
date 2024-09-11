4b1c36ce24e096ba74c3cf487297d9313966212c
Luis Pedraza <luisfedranol@gmail.com>
2024-09-11 15:49:09 -0600

    Se modifica el .gitignore y se crean los archivos de los ejercicios Numeros, TrianguloInvertido y Triangulo

---
+++ src/Triangulo.java
@@ -0,0 +1,12 @@
+
+public class Triangulo {
+	
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("* ");
+			} //for j
+			System.out.println("");
+		} //for i
+	}//main
+}//class Triangulo
